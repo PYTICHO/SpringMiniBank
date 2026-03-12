7889b7ef813f5f7a72ad8fe63821632327440269
PYTICHO <kasaev_2006@inbox.ru>
2026-03-12 14:12:46 +0300

    init

---
+++ src/main/java/com/bank/otp_bank/OtpBankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.otp_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtpBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/otp_bank/OtpBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.otp_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtpBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
